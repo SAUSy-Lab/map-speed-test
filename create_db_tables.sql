12d2542820d212a512489471c2ff31c0186f5c2b
Nate-Wessel <bike756@gmail.com>
2017-02-23 23:04:43 -0500

    adding structure of results table

--- create_db_tables.sql
+++ create_db_tables.sql
@@ -1,14 +1,17 @@
 -- table for points of origin and destination
 CREATE TABLE od (
 	id serial PRIMARY KEY,
-	vector geometry(LINE,4326)
+	vector geometry(LINESTRING,4326)
 );
 
-/*
--- tabe for storing results
+
+-- table for storing results
 CREATE TABLE od_results (
-	res_id serial, 
-	od_id integer,
-	
+	id serial PRIMARY KEY, 
+	od_id integer, -- id of od table
+	trace geometry(LINESTRING,4326),
+	start_time numeric, -- javascript time in milliseconds
+	end_time numeric, -- javascript time in milliseconds
+	zoom_level real,
+	map_extent geometry(POLYGON,4326) -- bounding box of map
 );
-*/
