0e6a579d8186fc34da6066662749fd3da5ce3dc8
Nate-Wessel <bike756@gmail.com>
2017-05-26 11:12:35 -0400

    moved server config details out of repository

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-# connection string from 
-# http://php.net/manual/en/function.pg-connect.php
-$DBconnectionString = "host=localhost port=5432 dbname=misc user=nate password=mink";
-?>
---
+++ sample_config.php
@@ -0,0 +1,7 @@
+<?php
+# enter your connection information and rename this file to config.php
+
+# connection string from 
+# http://php.net/manual/en/function.pg-connect.php
+$DBconnectionString = "host= port= dbname= user= password=";
+?>
